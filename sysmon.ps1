8cee337a0046d0a5458a081edece8097a0c5bdbd
Joseph Stahl <joe.stahl@gmail.com>
2017-02-27 20:59:09 -0500

    Added sysmon install capibility

---
+++ sysmon.ps1
@@ -0,0 +1,27 @@
+﻿
+function get-sysmon {
+
+$sysmondir = "c:\temp\sysmon\"
+
+if (!(test-path -Path $sysmondir)) {
+    mkdir $sysmondir
+}
+
+$sysmonurl = "https://download.sysinternals.com/files/Sysmon.zip"
+$savedzip = $sysmondir+"Sysmon.zip"
+
+(New-Object System.Net.WebClient).DownloadFile($sysmonurl, $savedzip)
+
+add-type -AssemblyName "system.io.compression.filesystem"
+
+[io.compression.zipfile]::ExtractToDirectory($savedzip, $sysmondir)
+
+cd $sysmondir
+}
+
+function install-sysmon {
+
+sysmon64.exe -i -n -accepteula 
+
+}
+
